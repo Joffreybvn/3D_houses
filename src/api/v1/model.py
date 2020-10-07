877446f174b089aa57e7f0be50a6e0f31fc9fc57
Joffrey <joffreybvn@gmail.com>
2020-10-07 15:48:19 +0200

    route parameters added

--- src/api/v1/model.py
+++ src/api/v1/model.py
@@ -7,11 +7,11 @@ from flask_restx import Namespace, Resource
 api = Namespace('model', description='3D model interactions')
 
 
-@api.route('')
+@api.route('<int:postal_code>/<string:street_name>/<string:house_number>')
 class Status(Resource):
 
     @api.response(200, "Success")
-    def get(self):
+    def get(self, postal_code: int, street_name: str, house_number: str):
         """
         Return the API server status.
         Return "Alive!" if this API server is online.
