f4646dbaa202398828004307fff4d0fc5cf2e253
Joffrey <joffreybvn@gmail.com>
2020-10-20 01:03:04 +0200

    House environment and UI reworked

--- assets/js/renderer.module.js
+++ assets/js/renderer.module.js
@@ -16,6 +16,8 @@ let init = (land, vegetation, house, offsets) => {
 
     // Scene definition
     scene = new THREE.Scene();
+    scene.background = new THREE.Color().setHSL( 0.6, 0, 1 );
+    scene.fog = new THREE.Fog( scene.background, 1, 5000 );
 
     // Camera's constants
     const fov = 70;
@@ -29,33 +31,67 @@ let init = (land, vegetation, house, offsets) => {
     // Camera controls
     controls = new TrackballControls(camera, renderer.domElement);
 
+    // Source: https://github.com/mrdoob/three.js/blob/dev/examples/webgl_lights_hemisphere.html
     // Lights
-    const light = new THREE.DirectionalLight( 0xaabbff, 0.3 );
-    light.position.x = 300;
-    light.position.y = 250;
-    light.position.z = - 500;
-    scene.add(light);
+    const hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
+    hemiLight.color.setHSL( 0.6, 1, 0.6 );
+    hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
+    hemiLight.position.set( 0, 50, 0 );
+    scene.add(hemiLight);
 
-    // Sky Dome
+    const dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
+    dirLight.color.setHSL( 0.1, 1, 0.95 );
+    dirLight.position.set( - 1, 1.75, 1 );
+    dirLight.position.multiplyScalar( 30 );
+    scene.add( dirLight );
+
+    dirLight.castShadow = true;
+    dirLight.shadow.mapSize.width = 2048;
+    dirLight.shadow.mapSize.height = 2048;
+
+    const d = 50;
+
+    dirLight.shadow.camera.left = - d;
+    dirLight.shadow.camera.right = d;
+    dirLight.shadow.camera.top = d;
+    dirLight.shadow.camera.bottom = - d;
+
+    dirLight.shadow.camera.far = 3500;
+    dirLight.shadow.bias = - 0.0001;
+
+    // Ground
+    const groundGeo = new THREE.PlaneBufferGeometry( 10000, 10000 );
+    const groundMat = new THREE.MeshLambertMaterial( {color: 0xffffff} );
+    groundMat.color.setHSL( 0.095, 1, 0.75 );
+
+    const ground = new THREE.Mesh( groundGeo, groundMat );
+    ground.position.y = - 33;
+    ground.rotation.x = - Math.PI / 2;
+    ground.receiveShadow = true;
+    scene.add( ground );
+
+    // Sky dome
     const vertexShader = document.getElementById( 'vertexShader' ).textContent;
     const fragmentShader = document.getElementById( 'fragmentShader' ).textContent;
     const uniforms = {
-        topColor: { value: new THREE.Color( 0x0077ff ) },
-        bottomColor: { value: new THREE.Color( 0xffffff ) },
-        offset: { value: 400 },
-        exponent: { value: 0.6 }
+        "topColor": { value: new THREE.Color( 0x0077ff ) },
+        "bottomColor": { value: new THREE.Color( 0xffffff ) },
+        "offset": { value: 33 },
+        "exponent": { value: 0.6 }
     };
-    uniforms.topColor.value.copy( light.color );
+    uniforms[ "topColor" ].value.copy( hemiLight.color );
 
-    const skyGeo = new THREE.SphereBufferGeometry(4000, 32, 15);
-    const skyMat = new THREE.ShaderMaterial({
+    scene.fog.color.copy( uniforms[ "bottomColor" ].value );
+
+    const skyGeo = new THREE.SphereBufferGeometry( 4000, 32, 15 );
+    const skyMat = new THREE.ShaderMaterial( {
         uniforms: uniforms,
         vertexShader: vertexShader,
         fragmentShader: fragmentShader,
         side: THREE.BackSide
-    });
+    } );
 
-    const sky = new THREE.Mesh(skyGeo, skyMat);
+    const sky = new THREE.Mesh( skyGeo, skyMat );
     scene.add(sky);
 
     // Object rotation
@@ -88,7 +124,8 @@ let init = (land, vegetation, house, offsets) => {
     LOADERS.ply(house, (geometry) => {
 
         // Set mesh color and set double side (avoid see through)
-        const material = new THREE.MeshPhongMaterial({color: 0xdddddd, specular: 0x009900, shininess: 30, flatShading: true, side: THREE.DoubleSide})
+        const material = new THREE.MeshPhongMaterial({color: 0x7c818b, specular: 0x7d4031, shininess: 10, flatShading: true, side: THREE.DoubleSide})
+        //const material = new THREE.MeshBasicMaterial( {color: 0x7c818b, side: THREE.DoubleSide} );
         const mesh = new THREE.Mesh(geometry, material);
 
         // Allow shadow and translate to (0, 0, 0)
