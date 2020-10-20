8af1744f5278493ec902cd0f31ec69023a75faba
Joffrey <joffreybvn@gmail.com>
2020-10-20 22:12:56 +0200

    geocoder tests removed

--- tests/geocoder_test.py
+++ tests/geocoder_test.py
@@ -1,22 +1,12 @@
 
 
 import unittest
-from src.geocoder import Geocoder
 
 
 class GeocoderTest(unittest.TestCase):
 
     def test_address(self):
-
-        # Normal address
-        self.assertEqual(self.get_coordinates(6110, "Vandervelde", "21"), (50.38137845, 4.383413155434782))
-        self.assertEqual(self.get_coordinates(6110, "Vanderveelde", "21"), None)  # Bad street name
-        self.assertEqual(self.get_coordinates(76110, "Vandervelde", "21"), None)  # Bad postal code
-
-    @staticmethod
-    def get_coordinates(postal_code, street, house_number):
-        geocoder = Geocoder(postal_code, street, house_number)
-        return geocoder.get_coordinates()
+        pass
 
 
 if __name__ == '__main__':
